f534b6cd1f349d2b4711d0b9b0330afd5f86ba0b
Jake Shelby <jake.shelby@me.com>
2014-06-01 12:54:41 -0600

    Cleanup

--- app/Proximo/Controllers/Webservice/MessageController.php
+++ app/Proximo/Controllers/Webservice/MessageController.php
@@ -26,46 +26,4 @@ class MessageController extends \Proximo\GenePool\Controller\Webservice\Root {
 	{
 	}
 
-
-/*
-    public function filterRequest($route, $request)
-    {
-        // Different from the regular auth filter, we'll
-        //  call the frontend service, in case they need
-        //  to check more things
-        if (!$this->service->loggedInCheck())
-            return Redirect::guest(route('user.login'));
-    }
-
-	public function getIndex()
-	{
-		$messages = $this->service->getUserMessages();
-		return View::make('proximo.dashboard', array(
-			'player' => $this->_getUser(),
-			'messages' => $messages,
-		));
-	}
-
-	public function postMessage()
-	{
-		$message = Input::get('content');
-		$lat = Input::get('latitude', 0);
-		$long = Input::get('longitude', 0);
-
-		try {
-			$this->service->userPostsMessage($message, $lat, $long);
-		} catch (Exception $e) {
-			$error = "Problem posting: ".$e->getMessage();
-		}
-		$redirect = Redirect::route('proximo.dashboard');
-		if (!empty($error)) {
-			$notice = $error;
-		} else {
-			$notice = "You have successfully posted the message";
-		}
-		$redirect->with('flash_notice', $notice);
-		return $redirect;
-	}
-*/
-
 }
--- app/Proximo/ProximoServiceProvider.php
+++ app/Proximo/ProximoServiceProvider.php
@@ -14,88 +14,15 @@ class ProximoServiceProvider extends \Illuminate\Support\ServiceProvider {
     public function boot()
     {
 
-
-/*
-		Route::get('/', array('as' => 'root', function () {
-
-			$man = App::make('proximo.manager');
-
-			\Log::info("session::all: " . print_r(Session::all(),true));
-
-			if (Session::has('proximo.id')) {
-
-				$username = Session::get('proximo.id');
-
-				//$lat = Input::get('latitude', Session::get('proximo.last_coords.lat', null));
-				//$long = Input::get('longitude', Session::get('proximo.last_coords.long', null));
-				$lat = Session::get('proximo.last_coords.lat', null);
-				$long = Session::get('proximo.last_coords.long', null);
-
-				$messages = null;
-				if (!is_null($lat) && !is_null($long)) {
-					$messages = $man->getMessagesNear($lat, $long);
-				}
-
-				return View::make('chat')
-					->with('username',$username)
-					->with('messages',$messages)
-				;
-
-			} else {
-
-				return View::make('beginning');
-
-			}
-
-
-		}));
-
-
-		Route::post('/login', array('as' => 'post.login', function () {
-
-			$id = Input::get('handle');
-
-			// TODO -- Should we Make sure the posted id is unique ???
-
-			Session::put('proximo.id', $id);
-
-			return \Redirect::route('root');
-
-		}));
-
-		Route::post('/postMessage', array('as' => 'post.postMessage', function () {
-
-			$man = App::make('proximo.manager');
-
-			$lat = Input::get('latitude', null);
-			$long = Input::get('longitude', null);
-			$message = Input::get('message', null);
-			$session = Session::get('_token');
-
-			Session::put('proximo.last_coords.lat', $lat);
-			Session::put('proximo.last_coords.long', $long);
-
-			$man->postMessage($session, $message, $lat, $long);
-
-			return \Redirect::route('root')->withInput();
-
-		}));
-*/
-
-
         // Routes
-
-
         Route::controller('auth',
 			'Proximo\Controllers\Frontend\AuthController');
-
 
         Route::controller('/',
 			'Proximo\Controllers\Frontend\IndexController');
 
         Route::controller('webservice/message',
 			'Proximo\Controllers\Webservice\MessageController');
-
 
     }
 
