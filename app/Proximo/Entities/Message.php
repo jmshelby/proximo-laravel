d8cb80496ccb5d6977becac23b88ec73c8e20b6c
Jake Shelby <jake.shelby@me.com>
2014-08-10 13:41:56 -0600

    Lat/Long accessors
    Because mongo requires a special object format

--- app/Proximo/Entities/Message.php
+++ app/Proximo/Entities/Message.php
@@ -50,4 +50,89 @@ $message->loc = array(
 		return $q->orderBy('created_at', 'desc');
 	}
 
+	// == Accessors ==============================================================
+
+	public function getLocationAttribute($value)
+	{
+		return $this->getLocation();
+	}
+
+	public function getLocAttribute($value)
+	{
+		return $this->getLocation();
+	}
+
+	public function setLocationAttribute($value)
+	{
+		$this->setLocAttribute($value);
+	}
+
+	public function setLocAttribute($value)
+	{
+		if (is_array($value)) {
+			$value = (object) $value;
+		}
+
+		$lat = null;
+		if (isset($value->lat)) {
+			$lat = $value->lat;
+		} elseif (isset($value->latitude)) {
+			$lat = $value->latitude;
+		}
+
+		$lng = null;
+		if (isset($value->long)) {
+			$lng = $value->long;
+		} elseif (isset($value->longitude)) {
+			$lng = $value->longitude;
+		}
+
+		$this->setLocation($lat, $lng);
+	}
+
+	// ===========================================================================
+
+	public function getLongitude()
+	{
+		$curLoc = $this->attributes['loc'];
+		if (isset($curLoc['coordinates'][0]))
+			return $curLoc['coordinates'][0];
+		else
+			return null;
+	}
+
+	public function getLatitude()
+	{
+		$curLoc = $this->attributes['loc'];
+		if (isset($curLoc['coordinates'][1]))
+			return $curLoc['coordinates'][1];
+		else
+			return null;
+	}
+
+	public function getLong() { return $this->getLongitude(); }
+	public function getLat() { return $this->getLatitude(); }
+
+	public function getLocation()
+	{
+		return (object) array(
+			'latitude' => $this->getLatitude(),
+			'longitude' => $this->getLongitude(),
+
+			'lat' => $this->getLatitude(),
+			'long' => $this->getLongitude(),
+		);
+	}
+
+	public function setLocation($newLat = null, $newLng = null)
+	{
+		$lat = !is_null($newLat) ? $newLat : $this->getLatitude();
+		$lng = !is_null($newLng) ? $newLng : $this->getLongitude();
+
+		$this->attributes['loc'] = array(
+			'type'			=> 'Point',
+			'coordinates'	=> array($lng, $lat),
+		);
+	}
+
 }
